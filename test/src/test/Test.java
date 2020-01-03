0956d09a66f52bf32718ffdb0b2292ce4d200d8a
SunQ <1500413160@qq.com>
2020-01-03 11:52:14 +0800

    a and b

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -4,6 +4,11 @@ public class Test {
 
 	public static void main(String[] args) {
 		
+		int a = 1;
+		int b = 2;
+		int c = a + b;
+		System.out.println(c);
+		
 		System.out.println("hello world");
 			
 	}
